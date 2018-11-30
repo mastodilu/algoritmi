699c8199cdb0347feb49a1f6d01fa382d0cf99a6
Matteo Di Lucchio <matteo.dilucchio@edu.unito.it>
2018-11-30 17:42:56 +0100

    recursive insertion sort

--- Esercizi/Esercizio1/Ordinamento/src/algoritmi/InsertionSort.java
+++ Esercizi/Esercizio1/Ordinamento/src/algoritmi/InsertionSort.java
@@ -6,69 +6,138 @@
  * ordinato correttamente.
  */
 package algoritmi;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  *
  * @author MatteoDiLucchio
  */
-public class InsertionSort<T extends Comparable<T>>{
-    private BufferedReader br;
-    private FileReader fr;
+public class InsertionSort<T extends Comparable>{
     /**
-     * path del file da aprire
+     * array disordinato
      */
-    private String path;
-    
+    ArrayList<T> array;
+    /**
+     * array che contiene solo elementi ordinati
+     */
+    ArrayList<T> sorted;
     
     /**
      * costruttore
      * @param path del file da aprire
      */
-    public InsertionSort(String PATH){
-        this.path = PATH;
-        this.open();
-        
-        System.out.println("File " + PATH + " opened correctly.");
+    public InsertionSort(ArrayList<T> array){
+        this.array = array;
+//        sorted = new ArrayList<T>();
+//        //aggiunge il primo elemento all'array ordinato come base di partenza
+//        sorted.add(array.get(0));
+//        array.remove(0);
     }
 
     /**
      * esegue l'ordinamento sul file
      */
-    public void sort() throws IOException{
-        String currentLine;
-        while ((currentLine = br.readLine()) != null) {
-            System.out.println(currentLine);
+    public void sort(){
+        System.out.println("Sorting started");
+        while(this.array.size() > 0){
+            this.addToOrdered(this.array.get(0));
+            this.array.remove(0);
         }
     }
     
     /**
-     * apre lo stream per la lettura del file
+     * aggiunge il parametro passato alla giusta posizione dell'array ordinato
+     * @param elem elemento da aggiungere nella giusta posizione
      */
-    private void open(){
-        try{
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
-        }catch(FileNotFoundException e){
-            System.err.println("Unable to open " + path);
-            this.close();
+    private void addToOrdered(T elem){
+        boolean added = false;
+        int i;
+        for(i = 0; i < this.sorted.size(); i++){
+            switch(elem.compareTo(this.sorted.get(i))){
+                case -1:
+                case 0:{
+                    //elem è minore di
+                    this.sorted.add(i, elem);
+                    added = true;
+                    break;
+                }
+                case 1: //elem è maggiore di
+                default:{
+                }
+            }
+        }
+        if(!added){
+            this.sorted.add(i, elem);
         }
     }
     
-    /**
-     * chiude lo stream per la lettura del file
-     */
-    public void close(){
-        try{
-            br.close();
-            fr.close();
-        }catch(IOException e){
-            System.err.println("Unable to close " + path);
+    
+    public void testSort(){
+        if(this.array.size() < 2){
+            return;
+        }
+    	int minIndex = 0;
+        //comincia a riordinare partendo dal secondo elemento dell'array
+        for(int i = 1; i < this.array.size(); i++){
+            testRecursiveSort(i, minIndex, i-1 );
         }
     }
+    
+
+    private void testRecursiveSort(int indexCurrentItem, int minIndex, int maxIndex){
+        int midIndex = (int)((minIndex + maxIndex)/2); //prende la parte intera
+        T currentItem = this.array.get(indexCurrentItem);
+        T midItem = this.array.get(midIndex);
+        
+        if(currentItem.compareTo(this.array.get(minIndex)) < 0){ // minore del minimo
+            this.array.remove(indexCurrentItem);
+            this.array.add(minIndex, currentItem);
+        }
+        else if(currentItem.compareTo(this.array.get(maxIndex)) > 0){ // maggiore del massimo
+            this.array.remove(indexCurrentItem);
+            this.array.add(maxIndex + 1, currentItem);
+        }
+        else if(currentItem.compareTo(midItem) == 0){ // uguale al medio
+            this.array.remove(indexCurrentItem);
+            this.array.add(midIndex, currentItem);
+        }
+        else switch(currentItem.compareTo(midItem)){
+            case -1:{
+                this.printArray(indexCurrentItem);
+                System.out.println("case -1 min " + minIndex + " mid " + midIndex + " max " + maxIndex + " item " + indexCurrentItem);
+                //l'elemento da ordinare e' minore di quello in posizione media. Confronto la parte a sinistra dell'indice medio
+                testRecursiveSort(indexCurrentItem, minIndex, midIndex-1);
+                break;
+            }
+            case 0:{ //caso gestito negli if-else iniziali
+                break;
+            }
+            case 1:{
+                this.printArray(indexCurrentItem);
+                System.out.println("case +1 min " + minIndex + " mid " + midIndex + " max " + maxIndex + " item " + indexCurrentItem);
+                //l'elemento da ordinare e' maggiore di quello in posizione media. Confronto la parte a destra dell'indice medio
+                //se confronto due indici consecutivi mi blocco e confronto, altrimenti continuo le chiamate ricorsive
+                testRecursiveSort(indexCurrentItem, midIndex+1, maxIndex);
+                break;
+            }
+            default:{
+                //...
+                System.err.println("You shouldn't read this.");
+                break;
+            }
+        }//-switch
+    }
+
+    private void printArray(int flagIndex) {
+        for(int i = 0; i < this.array.size(); i++){
+            System.out.print(this.array.get(i).toString() + " ");
+            if(i == flagIndex - 1)
+                System.out.print("# ");
+        }
+        System.out.println();
+    }
+    
+    
     
 }
--- Esercizi/Esercizio1/Ordinamento/src/ordinamento/MainClass.java
+++ Esercizi/Esercizio1/Ordinamento/src/ordinamento/MainClass.java
@@ -6,7 +6,12 @@
 package ordinamento;
 
 import algoritmi.InsertionSort;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  *
@@ -17,19 +22,98 @@ public class MainClass {
      * path del file da aprire
      */
     private static final String PATH = "C:\\Users\\masto\\Documents\\MieiProgetti\\Algoritmi_file_enormi\\Esercizio1\\integers.csv";
+//    private static final String PATH = "C:\\Users\\MatteoDiLucchio\\Documents\\Algoritmi\\Algoritmi file enormi\\Esercizio1\\integers.csv";
+    private static BufferedReader br;
+    private static FileReader fr;
+    private static ArrayList<Long> al;
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        InsertionSort is = new InsertionSort(PATH);
-        try{
-            is.sort();//avvia l'ordinamento del file
-            System.out.println("Sorting done with no errors");
-        }catch(IOException e){
-            System.err.println(e.getMessage());
+        
+        boolean shortTest = true;
+        if(shortTest){ //test con un array corto
+//            al = new ArrayList<Long>(Arrays.asList(1L,2L,2L,2L,3L,3L,4L,4L,5L,6L,8L,11L,1L));
+//            al = new ArrayList<Long>(Arrays.asList(5L,1L,2L,2L,2L,3L,3L,4L,4L,5L,6L,9L,8L));
+//            al = new ArrayList<Long>(Arrays.asList(1L,2L,2L,2L,3L,3L,4L,4L,5L,6L,8L,11L,1L));
+//            al = new ArrayList<Long>(Arrays.asList(10L,2L,2L,3L,1L,4L));
+//            al = new ArrayList<Long>(Arrays.asList(2L,4L,1L,3L));
+            al = new ArrayList<Long>(Arrays.asList(5L,10L,2L,2L,3L,1L,4L,1L,2L,20L,2L,3L,3L,4L,4L,5L,6L,9L,8L,1L,2L,2L,2L,3L,3L,4L,4L,5L,6L,8L,11L,1L,1L,2L,2L,2L,3L,3L,4L,4L,5L,6L,8L,11L,1L,10L,2L,2L,3L,1L,4L));
+
+        }else{//legge il file infinito di long
+            al = new ArrayList<Long>();
+            open();
+            read();
+            close();
         }
-        is.close();
+        
+        System.out.println(al.size() + " elementi da riordinare");
+        sort();
+        printArray();
     }
     
+    
+    /**
+     * stampa ogni numero contenuto nell'array
+     */
+    private static void printArray(){
+        for (int i = 0; i < al.size(); i++){
+            System.out.print(al.get(i) + " ");
+        }
+        System.out.println();
+
+    }
+    
+    
+    /**
+     * legge i numeri dal file e li aggiunge all'array
+     */
+    private static void read(){
+        String line;
+        try{
+            while( (line = br.readLine()) != null){
+                al.add(Long.parseLong(line));
+            }
+        }catch(IOException e){
+            System.err.println("Errore IO");
+        }
+    }
+    
+    
+    /**
+     * apre lo stream per la lettura del file
+     */
+    private static void open(){
+        try{
+            fr = new FileReader(PATH);
+            br = new BufferedReader(fr);
+        }catch(FileNotFoundException e){
+            System.err.println("Unable to open " + PATH);
+            close();
+        }
+    }
+    
+    
+    /**
+     * chiude lo stream per la lettura del file
+     */
+    private static void close(){
+        try{
+            br.close();
+            fr.close();
+        }catch(IOException e){
+            System.err.println("Unable to close " + PATH);
+        }
+    }
+
+
+	/**
+	  * avvia l'algoritmo di ordinamento
+	  */
+    private static void sort() {
+        InsertionSort<Long> is = new InsertionSort<Long>(al);
+//        is.sort();
+        is.testSort();
+    }
 }
