f4b4cf59eff2b335450f387a2b64e06f7d2f4f30
st148550 <di_lucchio.matteo@educ.di.unito.it>
2018-12-16 17:57:54 +0100

    addLink, removeLink

--- Esercizi/Esercizio1/Ordinamento/src/esercizio4/Node.java
+++ Esercizi/Esercizio1/Ordinamento/src/esercizio4/Node.java
@@ -37,7 +37,8 @@ public class Node<T> {
      * @param links
      * @param weights 
      */
-    public Node(T content,ArrayList<Node> links,ArrayList<Weight> weights){
+    public Node(T content,ArrayList<Node> links,ArrayList<Weight> weights) throws NodeException{
+        if(content == null || (links.size() != weights.size()) ) throw new NodeException("Content must be non null.");
         this.links = links;
         this.weights = weights;
         this.content = content;
@@ -49,8 +50,8 @@ public class Node<T> {
      * @param link
      * @param weight 
      */
-    public Node(T content,Node link,Weight weight){
-        
+    public Node(T content,Node link,Weight weight) throws NodeException{
+        if(content == null ) throw new NodeException("Content must be non null.");
         this.links = new ArrayList<Node>();
         this.links.add(link);
         this.weights = new ArrayList<Weight>();
@@ -68,5 +69,43 @@ public class Node<T> {
     
     public T getContent(){
         return this.content;
+    }
+    
+    /**
+     * Aggiunge un collegamento con peso ad un altro nodo.
+     * @param n il nodo a cui punta il collegamento
+     * @param w il peso del collegamento
+     * @throws NodeException se un parametro e' null
+     */
+    public void addLink(Node n, Weight w) throws NodeException{
+        if(n == null || w == null) throw new NodeException("Node and Content must be non null.");
+        this.links.add(n);
+        this.weights.add(w);
+    }
+    
+    /**
+     * Aggiunge una serie di collegamenti pesati al nodo corrente.
+     * @param nodes arraylist di nodi.
+     * @param weights arraylist di pesi.
+     * @throws NodeException se c'e' un valore null o se nodi e pesi hanno lunghezze diverse.
+     */
+    public void addLink(ArrayList<Node> nodes, ArrayList<Weight> weights) throws NodeException{
+        if(nodes.size() != weights.size()) throw new NodeException("Each link has to be a weight.");
+        for(int i = 0; i < nodes.size(); i++){
+            if(nodes.get(i) == null)    throw new NodeException("Node must be non null.");
+            if(weights.get(i) == null)  throw new NodeException("Weight must be non null.");
+            this.links.add(nodes.get(i));
+            this.weights.add(weights.get(i));
+        }
+    }
+    
+    /**
+     * Rimuove il collegamento al nodo specificato.
+     * @param n il nodo a cui e' collegato il nodo corrente.
+     */
+    public void removeLink(Node n){
+        int index = this.links.indexOf(n);
+        this.links.remove(index);
+        this.weights.remove(index);
     }
 }
